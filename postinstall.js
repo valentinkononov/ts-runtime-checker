8709782ac767d202cddf489221a6ed508e880b57
valentinkononov <valentin.kononov@mapbox.com>
2020-02-17 13:13:35 +0300

    feat(docs): add postinstall script with feedback notice in npm install log

---
+++ postinstall.js
@@ -0,0 +1,16 @@
+function isTrue(value) {
+    return !!value && value !== '0' && value !== 'false';
+}
+
+const envDisable = isTrue(process.env.CI);
+const logLevel = process.env.npm_config_loglevel;
+const logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
+
+if (!envDisable && !logLevelDisplay) {
+    const pkg = require(require('path').resolve('./package.json'));
+    if (pkg.collective) {
+        console.log(`\u001b[96m\u001b[1mThank you for using ${pkg.name}!\u001b[96m\u001b[1m`);
+        console.log(`\u001b[0m\u001b[96mIf you tried this package, please consider giving usage feedback:\u001b[22m\u001b[39m`);
+        console.log(`> \u001b[94m${pkg.issues}\u001b[0m\n`);
+    }
+}
