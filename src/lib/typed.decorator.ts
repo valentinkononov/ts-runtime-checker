b2235c3205788c2feb86bd7627df9ceccd88b5ad
valentinkononov <valentin.kononov@mapbox.com>
2020-02-29 18:29:02 +0300

    fet(core): add global config for TYPED decorator

--- src/lib/typed.decorator.ts
+++ src/lib/typed.decorator.ts
@@ -1,30 +1,32 @@
 import 'reflect-metadata';
+import { TypedConfig, TypedOptions } from './typed.config';
 
 // target = class
 // propertyName = function
 // descriptor = what a function
-
-export interface TypedConfig {
-  throwError?: boolean;
-  checkLength?: boolean;
-}
 
 /***
  * Typed decorator checks function arguments amount and types in runtime
  * In case if caller used incorrect types in runtime, Error will be thrown
  * */
 // tslint:disable-next-line:ban-types
-export function Typed(config: TypedConfig = { throwError: true, checkLength: true }) {
-  // tslint:disable-next-line:ban-types
+export function Typed(config?: TypedOptions) {
+    // tslint:disable-next-line:ban-types
   return (target: Object, propertyName: string, descriptor: TypedPropertyDescriptor<Function>) => {
-    const method = descriptor.value;
+      const method = descriptor.value;
     descriptor.value = function() {
-      const paramTypes = Reflect.getMetadata('design:paramtypes', target, propertyName);
-      if (config.checkLength) {
+        const options: TypedOptions = config || TypedConfig.get();
+        if (!options.enable) {
+            return method.apply(this, arguments);
+        }
+
+        const paramTypes = Reflect.getMetadata('design:paramtypes', target, propertyName);
+
+        if (options.checkArgumentLength) {
         // 1 check length of arguments and parameters
         if (arguments.length !== paramTypes.length) {
           const errorMessage = `Parameters length: ${paramTypes.length} is different from arguments length: ${arguments.length}`;
-          if (config.throwError) {
+          if (options.throwError) {
             throw new Error(errorMessage);
           } else {
             // tslint:disable-next-line:no-console
@@ -49,7 +51,7 @@ export function Typed(config: TypedConfig = { throwError: true, checkLength: tru
 
         if (actualType !== expectedType) {
           const errorMessage = `Argument: ${arguments[i]} has type: ${actualType} different from expected type: ${expectedType}.`;
-          if (config.throwError) {
+          if (options.throwError) {
             throw new Error(errorMessage);
           } else {
             // tslint:disable-next-line:no-console
