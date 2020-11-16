d215cd6138b90648357981def1d6c434f3e07c38
valentinkononov <valentin.kononov@mapbox.com>
2020-11-16 09:49:22 +0400

    feat(ci): fix dependencies

--- src/lib/typed.utils.ts
+++ src/lib/typed.utils.ts
@@ -49,5 +49,6 @@ export const logOrThrowIncorrectArgument = (
 };
 
 export interface Type<T> extends Function {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     new (...args: any[]): T;
 }
